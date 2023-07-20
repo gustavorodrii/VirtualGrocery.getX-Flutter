ac3651bb8bcd074cfc95f48fbae724cc071f1cee
Gustavo <gugarodri@hotmail.com>
2023-07-20 15:21:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../src/auth/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Virtual Grocery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
